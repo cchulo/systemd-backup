a13ca7084a0bf706a6735c6b92f3e9810199bad9
Carlos Chulo <cchulo.dev@gmail.com>
2023-07-07 18:59:51 -0700

    final touches for now

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 set -e -o pipefail
 
+echo
 if [[ "$1" == "local" ]]; then
   echo "Installing scripts in ${HOME}/.local"
   BASE_BIN="${HOME}/.local/bin"
@@ -18,12 +19,24 @@ else
   PERMISSION="sudo"
 fi
 
+echo
 echo 'Installing systemd-backup'
 ${PERMISSION} cp backup ${BASE_BIN}
 cp backup.service ${HOME}/.config/systemd/user/
 cp backup.timer ${HOME}/.config/systemd/user/
 
+set +e
+set +o pipefail
+
+mkdir -p ${HOME}/.config/systemd-backup/
+
+cp -n backup.json ${HOME}/.config/systemd-backup/ 2>/dev/null
+cp -n exclude ${HOME}/.config/systemd-backup/ 2>/dev/null
+touch ${HOME}/.config/systemd-backup/.secret
+
 systemctl --user daemon-reload
 
+echo
 echo 'systemd-backup installed!'
-echo 'to activate execute `systemctl --user enable --now backup.timer`'
+echo
+echo 'To activate execute `systemctl --user enable --now backup.timer`'
